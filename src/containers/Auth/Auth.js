9dae22dd5a41f4fdf703a9d821bfb67026707638
ayan <ayansome1@gmail.com>
2018-02-18 14:27:08 +0530

    login button auth fixed

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import FacebookLogin from 'react-facebook-login';
 
-
-import Input from '../../components/UI/Input/Input';
-import Button from '../../components/UI/Button/Button';
-import Spinner from '../../components/UI/Spinner/Spinner';
-import classes from './Auth.css';
+// import Input from '../../components/UI/Input/Input';
+// import Button from '../../components/UI/Button/Button';
+// import Spinner from '../../components/UI/Spinner/Spinner';
+// import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { updateObject, checkValidity } from '../../shared/utility';
 
@@ -18,80 +17,87 @@ class Auth extends Component {
                 elementType: 'input',
                 elementConfig: {
                     type: 'email',
-                    placeholder: 'Mail Address'
+                    placeholder: 'Mail Address',
                 },
                 value: '',
                 validation: {
                     required: true,
-                    isEmail: true
+                    isEmail: true,
                 },
                 valid: false,
-                touched: false
+                touched: false,
             },
             password: {
                 elementType: 'input',
                 elementConfig: {
                     type: 'password',
-                    placeholder: 'Password'
+                    placeholder: 'Password',
                 },
                 value: '',
                 validation: {
                     required: true,
-                    minLength: 6
+                    minLength: 6,
                 },
                 valid: false,
-                touched: false
-            }
+                touched: false,
+            },
         },
-        isSignup: true
-    }
+        isSignup: true,
+    };
 
-    componentDidMount () {
-        if ( !this.props.buildingBurger && this.props.authRedirectPath !== '/' ) {
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
             this.props.onSetAuthRedirectPath();
         }
     }
 
-    inputChangedHandler = ( event, controlName ) => {
-        const updatedControls = updateObject( this.state.controls, {
-            [controlName]: updateObject( this.state.controls[controlName], {
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
-                valid: checkValidity( event.target.value, this.state.controls[controlName].validation ),
-                touched: true
-            } )
-        } );
-        this.setState( { controls: updatedControls } );
-    }
+                valid: checkValidity(
+                    event.target.value,
+                    this.state.controls[controlName].validation
+                ),
+                touched: true,
+            }),
+        });
+        this.setState({ controls: updatedControls });
+    };
 
-    submitHandler = ( event ) => {
+    submitHandler = event => {
         event.preventDefault();
-        this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup );
-    }
+        this.props.onAuth(
+            this.state.controls.email.value,
+            this.state.controls.password.value,
+            this.state.isSignup
+        );
+    };
 
     switchAuthModeHandler = () => {
-        this.setState( prevState => {
+        this.setState(prevState => {
             return { isSignup: !prevState.isSignup };
-        } );
-    }
+        });
+    };
 
-    responseFacebook = (response) => {
-      console.log(response);
-    }
+    responseFacebook = response => {
+        console.log(response);
+    };
 
-    componentClicked = (response) => {
-      console.log(response);
-    }
+    componentClicked = response => {
+        console.log(response);
+    };
 
-    render () {
+    render() {
         const formElementsArray = [];
-        for ( let key in this.state.controls ) {
-            formElementsArray.push( {
+        for (let key in this.state.controls) {
+            formElementsArray.push({
                 id: key,
-                config: this.state.controls[key]
-            } );
+                config: this.state.controls[key],
+            });
         }
 
-        let form = formElementsArray.map( formElement => (
+/*        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -100,35 +106,36 @@ class Auth extends Component {
                 invalid={!formElement.config.valid}
                 shouldValidate={formElement.config.validation}
                 touched={formElement.config.touched}
-                changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
-        ) );
+                changed={event => this.inputChangedHandler(event, formElement.id)}
+            />
+        ));*/
 
-        if ( this.props.loading ) {
-            form = <Spinner />
-        }
+/*        if (this.props.loading) {
+            form = <Spinner />;
+        }*/
 
-        let errorMessage = null;
+        // let errorMessage = null;
 
-        if ( this.props.error ) {
-            errorMessage = (
-                <p>{this.props.error.message}</p>
-            );
-        }
+        // if (this.props.error) {
+        //     errorMessage = <p>{this.props.error.message}</p>;
+        // }
 
-        let authRedirect = null;
-        if ( this.props.isAuthenticated ) {
-            authRedirect = <Redirect to={this.props.authRedirectPath} />
-        }
+        // let authRedirect = null;
+        // if (this.props.isAuthenticated) {
+        //     authRedirect = <Redirect to={this.props.authRedirectPath} />;
+        // }
 
         return (
-
             <div>
                 <FacebookLogin
                     appId="339967319744088"
                     autoLoad={false}
-                    fields="name,email,picture"
+                    fields="name,email"
+                    scope="public_profile,user_friends"
+                    // scope="public_profile,user_friends,user_photos"
                     onClick={this.componentClicked}
-                    callback={this.responseFacebook} />
+                    callback={this.responseFacebook}
+                />
             </div>
 
             /*<div className={classes.Auth}>
@@ -152,15 +159,15 @@ const mapStateToProps = state => {
         error: state.auth.error,
         isAuthenticated: state.auth.token !== null,
         buildingBurger: state.burgerBuilder.building,
-        authRedirectPath: state.auth.authRedirectPath
+        authRedirectPath: state.auth.authRedirectPath,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
-        onSetAuthRedirectPath: () => dispatch( actions.setAuthRedirectPath( '/' ) )
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/')),
     };
 };
 
-export default connect( mapStateToProps, mapDispatchToProps )( Auth );
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
