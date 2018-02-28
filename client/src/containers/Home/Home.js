20feba43e8d7c3f7793bf5c164f69f2202a39669
ayan <ayansome1@gmail.com>
2018-02-28 16:42:18 +0530

    fetched location

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux/Aux';
+// import Aux from '../../hoc/Aux/Aux';
 
 class Home extends Component {
 
+	state = {loadingLocation:true,locationAvailability : false};
+
+	componentDidMount(){
+		const showPosition = (position) => {
+
+			 this.setState({locationAvailability:true,
+							loadingLocation:false,
+							lat:position.coords.latitude,
+							lng:position.coords.longitude});
+		};
+
+		 const getLocation = () => {
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(showPosition);
+		    } else { 
+				this.setState({loadingLocation:false,locationAvailability:false});
+		    	console.log("Geolocation is not supported by this browser.");
+		    }
+		}
+
+		getLocation();
+	}
+
     render () {
-        return (
-            <Aux>
-               this is home page
-            </Aux>
-        );
+
+    	if(this.state.loadingLocation){
+    		return(<div>loading location</div>);
+    	}
+    	else if(!this.state.locationAvailability){
+    		return(<div>Geolocation is not supported by this browser.</div>);
+    	}
+    	else{
+    		return(
+    			<div>
+    				Lat:{this.state.lat} 
+    				<br/> 
+    				Long:{this.state.lng}
+    			</div>
+    			);    		
+    	}
     }
 }
 
