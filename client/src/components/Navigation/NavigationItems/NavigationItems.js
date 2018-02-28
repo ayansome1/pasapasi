1be1ddf97ae6da00fabb7cf5076943019d25d147
ayan <ayansome1@gmail.com>
2018-02-28 13:16:54 +0530

    redirect to login after logout

--- client/src/components/Navigation/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,61 +1,29 @@
 import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom';
-
-
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
-
 import axios from 'axios';
-
-// const navigationItems = ( props ) => (
-//     <ul className={classes.NavigationItems}>
-//         <NavigationItem link="/" exact>Home</NavigationItem>
-//         <NavigationItem link="/profile" exact>Profile</NavigationItem>
-//         <NavigationItem link="/login">Logout</NavigationItem>
-//     </ul>
-// );
-
-// export default navigationItems;
-
 
 class NavigationItems extends Component {
 
-	state = {
-	    isLoggedOut: false,
-	    isLoading: true
-	}	
-
-
-  componentDidMount () {
-
-  }
-
 
   logout = (event) => {
-  	console.log("logout");
   	axios.get('/logout').then(()=>{
-  		this.setState({isLoggedOut: true,isLoading:false})
+  		window.location = "/login";
   	}).catch(()=>{
-  		this.setState({isLoading:false})
+  		console.log("ERROR");
   	});
   } 
 
   render(){
 
-	  	if(this.state.isLoggedOut){
-	          return <Redirect to="/login" />
-	  	}
-	  	else{
-		    return (
-			    <ul className={classes.NavigationItems}>
-			        <NavigationItem link="/" exact>Home</NavigationItem>
-			        <NavigationItem link="/profile" exact>Profile</NavigationItem>
-			        <button onClick={this.logout}>Logout</button>
-			    </ul>
+	    return (
+		    <ul className={classes.NavigationItems}>
+		        <NavigationItem link="/" exact>Home</NavigationItem>
+		        <NavigationItem link="/profile" exact>Profile</NavigationItem>
+		        <button onClick={this.logout}>Logout</button>
+		    </ul>
 
-		  	);
-	  	}
-
+	  	);
   }
 
 
