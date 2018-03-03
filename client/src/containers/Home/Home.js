a3e6726c107dac7df4637977e1b82be0eec6ca21
ayan <ayansome1@gmail.com>
2018-03-03 14:31:37 +0530

    grid done

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Bootstrap, Grid, Row, Col } from 'react-bootstrap';
 
 class Home extends Component {
 	state = { loading: true };
@@ -42,59 +41,23 @@ class Home extends Component {
 		if (this.state.loading) {
 			return <div>Loading...</div>;
 		} else {
-
-			return (
-				<Grid>
-							
-						{this.state.people.map((item, index) => {
-							
-									return <Row className="show-grid">
-										<Col xs={12} sm={6} key={index}>
-											{item.first_name}
-											<br />
-											<a href={item.fb_link} target="_blank">
-												fb profile
-											</a>
-											<br />
-											{item.distance}
-											<br />
-											{item.gender}
-											<br />
-											{item.last_active}
-										</Col>
-									</Row>
-						})}
-
-
-
-
-				</Grid>
-			);
-
-			/*return this.state.people.map((item, index) => {
+			return this.state.people.map((item, index) => {
 				return (
-					<Grid>
-						<Row className="show-grid">
-							<Col xs={3} sm={3} key={index}>
-								{item.first_name}
-								<br />
-								<a href={item.fb_link} target="_blank">
-									fb profile
-								</a>
-								<br />
-								{item.distance}
-								<br />
-								{item.gender}
-								<br />
-								{item.last_active}
-							</Col>
-						</Row>
-					</Grid>
+					<div className="col-xs-12 col-sm-4" key={index}>
+						{item.first_name}
+						<br />
+						<a href={item.fb_link} target="_blank">
+							fb profile
+						</a>
+						<br />
+						{item.distance}
+						<br />
+						{item.gender}
+						<br />
+						{item.last_active}
+					</div>
 				);
-			});*/
-
-
-
+			});
 		}
 	}
 }
